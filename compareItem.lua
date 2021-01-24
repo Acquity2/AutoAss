3379670637d0e895ba46cf10f4407ed8fd04eb9a
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-01-24 23:47:02 +0800

    Add files via upload

--- compareItem.lua
+++ compareItem.lua
@@ -7,7 +7,9 @@ local md5List = require('md5List')
 compareItem = {}
 
 function compareItem.caculateMd5(fileLoc)
-	MD5 = md5.sumhexa(fileLoc)
+	local file = io.open(fileLoc)
+	local content = file:read('*a') 
+	local MD5 = md5.sumhexa(content)
 	return MD5
 end
 
