04e64ae5fcd265c2dfceccbc303ba08cac6e08e4
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-01-26 23:16:53 +0800

    Add files via upload

--- liquidRecipe.lua
+++ liquidRecipe.lua
@@ -22,4 +22,19 @@ liquidRecipe[2] = {
 	}
 }
 
+liquidRecipe[3] = {
+	{
+		type = "solder" ,
+		amount = "144",
+	},	
+	{
+		type = "lubricating_oil",
+		amount = "250",
+	},
+	{
+		type = "rubber",
+		amount = "1440",
+	}
+}
+
 return liquidRecipe
--- md5List.lua
+++ md5List.lua
@@ -2,6 +2,7 @@ md5List = {}
 md5List[0] = "464ced077dcf85e81c40a72a3c62ed73" --empty
 md5List[1] = "f13f3531e58bd60f5bb8e553edf92461" --luv 马达
 md5List[2] = "e0d9e552dd5e91e28035bb144e378f7c" --luv 泵
+md5List[3] = "4b5e7a1b5fbbed735b9480ddc5bf3e91" --luv 传送带
 
 
 
