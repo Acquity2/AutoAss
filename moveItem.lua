df3c557a735295fe6e96a554deb93e1b26316100
Acquity2 <54288089+Acquity2@users.noreply.github.com>
2021-02-07 19:10:58 +0800

    Add files via upload

--- moveItem.lua
+++ moveItem.lua
@@ -50,7 +50,7 @@ function moveItem.Main()
 end
 
 function moveItem.rocketMain()
-	print(it's a rocket recipe!!!)
+	print("it's a rocket recipe!!!")
 	robot.select(1)
 	local Now = 1
 	local Target = 1
